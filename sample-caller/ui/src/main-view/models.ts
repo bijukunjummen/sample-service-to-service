406499cd58b656ade8ce2992fcaffdb96897815b
Biju Kunjummen <biju.kunjummen@gmail.com>
2021-11-16 10:57:41 -0800

    Fixes UI

--- sample-caller/ui/src/main-view/models.ts
+++ sample-caller/ui/src/main-view/models.ts
@@ -3,14 +3,20 @@ export interface MessageAck {
     received: string;
     callerHeaders: Map<string, string[]>;
     producerHeaders: Map<string, string[]>;
+    callerMetadata: ClusterMetadata;
+    producerMetadata: ClusterMetadata;
     statusCode: number;
     roundTripTimeMillis: number;
 }
-
 
 export interface Message {
     id?: string;
     payload: string;
     delay: number;
     responseCode: number;
+}
+
+export interface ClusterMetadata {
+    clusterName: string;
+    clusterLocation: string;
 }
