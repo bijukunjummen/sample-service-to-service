1d4dcec9c193ee97bf9eeb389bc889dee1d14c97
Biju Kunjummen <biju.kunjummen@gmail.com>
2023-02-17 22:44:29 -0800

    Changed namespace to default

--- sample-producer/src/main/kotlin/sample/producer/SampleProducerApp.kt
+++ sample-producer/src/main/kotlin/sample/producer/SampleProducerApp.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SampleServiceApp
+class SampleProducerApp
 
 fun main(args: Array<String>) {
-    runApplication<SampleServiceApp>(*args)
+    runApplication<SampleProducerApp>(*args)
 }
