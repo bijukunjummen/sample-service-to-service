476714cf77ab4ebfefe66f3bfc6d350bac65558f
Biju Kunjummen <biju.kunjummen@gmail.com>
2024-05-25 14:33:56 +0530

    Upgraded dashboard

---
+++ sample-caller/src/main/kotlin/sample/caller/config/MetricConfig.kt
@@ -0,0 +1,20 @@
+package sample.caller.config
+
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class MetricConfig {
+    @Bean
+    fun configurer(
+        @Value("\${spring.application.name}") applicationName: String
+    ): MeterRegistryCustomizer<MeterRegistry> {
+        return MeterRegistryCustomizer { registry: MeterRegistry ->
+            registry.config().commonTags("application", applicationName)
+        }
+    }
+}
